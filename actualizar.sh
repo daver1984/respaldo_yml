bf14db71b7b59afce1194a87e375e3c11367880d
daver1984.dante <daver1984.dant.verdugo@duocuc.cl>
2024-10-26 22:16:08 -0300

    Backup de archivo de actualizaciones

---
+++ actualizar.sh
@@ -0,0 +1,33 @@
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sudo docker stop portainer
+sudo docker rm portainer
+sudo docker pull portainer/portainer-ce
+sudo docker run -d --name=portainer --hostname=portainer --network=host --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -e TZ='America/Santiago' portainer/portainer-ce
+cd /home/pi/docker/plex
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/jellyfin
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/homeassistant
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/mysql
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/duckdns
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/wgeasy
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+#sudo shutdown -r now
