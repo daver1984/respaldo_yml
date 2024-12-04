f6d020e5b75284475a80a00b1cfbd8ed2cacc905
daver1984 <daver1984.dant.verdugo@duocuc.cl>
2024-12-04 15:37:08 -0300

    Se cambia la tecnologia anterior por una nueva

--- actualizar.sh
+++ actualizar.sh
@@ -2,23 +2,7 @@ sudo apt-get update -y
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
-sudo docker stop portainer
-sudo docker rm portainer
-sudo docker pull portainer/portainer-ce
-sudo docker run -d --name=portainer --hostname=portainer --network=host --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -e TZ='America/Santiago' portainer/portainer-ce
-cd /home/pi/docker/plex
-sudo docker compose pull
-sudo docker compose up -d --remove-orphans
-sudo docker image prune -f
-cd /home/pi/docker/jellyfin
-sudo docker compose pull
-sudo docker compose up -d --remove-orphans
-sudo docker image prune -f
-cd /home/pi/docker/homeassistant
-sudo docker compose pull
-sudo docker compose up -d --remove-orphans
-sudo docker image prune -f
-cd /home/pi/docker/mysql
+cd /home/pi/docker/bot
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans
 sudo docker image prune -f
@@ -26,19 +10,30 @@ cd /home/pi/docker/duckdns
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans
 sudo docker image prune -f
-cd /home/pi/docker/wgeasy
+cd /home/pi/docker/homeasistant
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans
 sudo docker image prune -f
-cd /home/pi/docker/netdata
-sudo docker compose pull
-sudo docker compose up -d --remove-orphans
-sudo docker image prune -f
-cd /home/pi/docker/bot
+cd /home/pi/docker/jellyfin
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans
 sudo docker image prune -f
 cd /home/pi/docker/minecraft
 sudo docker compose pull && sudo docker compose down && sudo docker compose up -d --remove-orphans
 sudo docker image prune -f
-#sudo shutdown -r now
+cd /home/pi/docker/netdata
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/plex
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/portainer
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
+cd /home/pi/docker/wg-pihole
+sudo docker compose pull
+sudo docker compose up -d --remove-orphans
+sudo docker image prune -f
