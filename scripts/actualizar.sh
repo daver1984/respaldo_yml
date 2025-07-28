d6c3b61441e66f78ffbb1e59591c0dc775a730ba
Dante Verdugo <dante.verdugo@ssff.cl>
2025-07-27 21:05:29 -0400

    Se actualiza carpeta de respaldos agregando codigo automatizado

--- scripts/actualizar.sh
+++ scripts/actualizar.sh
@@ -1,7 +1,12 @@
+# Archivo de actualización de sistemas.
+
+#Actualizando Raspbian:
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
+
+#Actualizando los Dockers:
 cd /home/pi/docker/bot
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans
@@ -30,4 +35,15 @@ cd /home/pi/docker/wire-adguard
 sudo docker compose pull
 sudo docker compose up -d --remove-orphans
 sudo docker image prune -f
+
+#Verificacion de archivos antes del reinicio:
+echo "Verificando servicios activos antes de reiniciar..." >> "$LOGFILE"
+systemctl list-units --type=service --state=running >> "$LOGFILE"
+
+# Envía el correo de confirmación
+echo "✅ Reinicio ejecutado exitosamente el $(date)" | msmtp --subject="Confirmación desde Raspberry Pi" verdugoper@gmail.com
+
+# Mensaje y retraso antes del reinicio
+echo "Reiniciando en 30 segundos..." >> "$LOGFILE"
+sleep 30
 sudo reboot
