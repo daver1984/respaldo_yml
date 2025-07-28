4ad69344b12122eb8584020fa6320391d26f432a
daver1984 <daver1984.dant.verdugo@duocuc.cl>
2025-07-28 12:43:10 -0400

    Se dejan corregidos los script de actualizacion

--- scripts/arranque.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sleep 60  # Da tiempo a que la red se estabilice
-
-echo -e "Subject: 🟢 Raspberry Pi encendida correctamente\n\nHola Dante,\n\nLa Raspberry Pi inició con éxito el $(date).\nIP actual: $(hostname -I)\nUptime: $(uptime -p)\n\nSaludos, el bot del reboot semanal 🍃" | msmtp verdugoper@gmail.com
